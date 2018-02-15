964aa22ecedc4f50278cd36a39a5b7eb0e1d456b
Hehkpop <bermagambet.d@gmail.com>
2018-02-15 13:24:25 +0600

    Lab2
    Lab2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle Radius = new Circle(int.Parse(Console.ReadLine()));
+            Console.WriteLine(Radius.FindCircumference() + " and " + Radius.findDiameter());
+            Console.ReadKey();
+
+            List<Student> students = new List<Student>();
+            string s = Console.ReadLine();
+            string[] names = s.Split(' ');
+            for (int i = 0; i < names.Length; i++ )
+            {
+
+                Student student = new Student(names[i], names[i+1]);
+                student.gpa = double.Parse(names[i+2]);
+                students.Add(student);
+                i += 2;
+            }
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Name + " " + student.Surname + " " + student.gpa);
+            }
+            Console.ReadKey();
+        }
+    }
+}
